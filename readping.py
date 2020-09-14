d6f22a63d9331c6ea47e2b2b97366daf4f6d1a56
Tate Cantrell <tate@mocamail.com>
2020-09-14 17:11:55 +0100

    flexible padding so that alerts line up

--- readping.py
+++ readping.py
@@ -15,7 +15,14 @@ import sys
 import re
 from time import localtime, gmtime, strftime
 
-# this lines things up with the icmp_seq (todo - I should update to actually use the icmp_seq instead of an iterative)
+# adjustible variables
+HIGH_MULTIPLE = 4
+separator = "="
+info_width = 60
+
+# program variables preset
+# this lines things up with the icmp_seq 
+# TODO - I should update to actually use the icmp_seq instead of an iterative)
 iterations = -2
 pingtime = 0.0
 cumsum = 0.0
@@ -23,7 +30,6 @@ average = 0.0
 alert_num = 0
 alert_total = 0
 alert_active = False
-HIGH_MULTIPLE = 5
 alert_buffer = ""
 
 for line in sys.stdin:
@@ -37,12 +43,12 @@ for line in sys.stdin:
 			cumsum += pingtime
 			average = cumsum / iterations
 		currtime = strftime("%d %b %Y %H:%M:%S", localtime())
-		alert = "Alert {} pings run - {:6.1f} ms - {:6.1f} ms average - {}".format(iterations, pingtime, average, currtime)
+		alert = "Alert {} pings run - {:6.1f} ms - {}".format(iterations, pingtime, currtime)
 		if pingtime > HIGH_MULTIPLE * average:
 			alert_num += 1
 			alert_total += 1
 			if(alert_num == 1):
-				print("=========================================================")
+				print(separator * info_width)
 				print(alert_buffer)
 			print(alert)
 			alert_active = True
@@ -54,15 +60,20 @@ for line in sys.stdin:
 			alert_num = 0
 			alert_buffer = alert
 			if alert_active:
-				print("")
-				print("======================= {} Alerts =======================".format(alert_total))
+				alert_print = " {} Alerts - {:.1f} ms average ".format(alert_total, average)
+				alert_pad = info_width - len(alert_print)
+				alert_left_pad = separator * int(alert_pad / 2)
+				alert_right_pad = alert_left_pad
+				if alert_pad % 2 != 0:
+					alert_right_pad += separator
+				print(alert_left_pad + alert_print + alert_right_pad)
 				print("")
 				alert_active = False
 		if iterations % 1000 == 0 or iterations == 10 or iterations == 100 or iterations == 500 and iterations > 0:
 			print("")
-			print("=========================================================")
-			print ("{} pings run - {:6.1f} ms average - {} alerts".format(iterations, average, alert_total))
-			print("=========================================================")
+			print(separator * info_width)
+			print ("{} pings run - {:.1f} ms average - {} alerts".format(iterations, average, alert_total))
+			print(separator * info_width)
 			print("")
 	else:
 		other_msg = re.search(r".*", line)
